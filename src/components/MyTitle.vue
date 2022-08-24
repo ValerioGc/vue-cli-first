f619bfe82544d7f04d0587d3481ef58d1a606eb0
ValerioGc <valerio.gc1@gmail.com>
2022-08-24 09:42:58 +0200

    Fine esercizio

---
+++ src/components/MyTitle.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="MyTitle">
+        <h1 class="center">La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script></script>
+
+<style lang="scss">
+    .center {text-align: center; color: red;}
+</style>
